e19df34bfca96a214e6658129422d8d1d09bb9d7
FrancoiseSagan <2537953899@qq.com>
2025-06-21 16:23:30 +0800

    parser

--- app/Main.hs
+++ app/Main.hs
@@ -8,8 +8,40 @@ symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 spaces :: Parser ()
 spaces = skipMany space
 
+data LispVal = Atom String
+                | List [LispVal]
+                | DottedList [LispVal] LispVal
+                | Number Integer
+                | String String
+                | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do
+                char '"'
+                x <- many $ noneOf "\""
+                char '"'
+                return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+                first <- letter <|> symbol
+                rest <- many (letter <|> symbol <|> digit)
+                let atom = first : rest
+                return $ case atom of
+                        "#t" -> Bool True
+                        "#f" -> Bool False
+                        _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = do
+                digits <- many1 digit
+                return $ Number $ read digits
+
+parseExpr :: Parser LispVal
+parseExpr = parseString <|> parseAtom <|> parseNumber
+
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of 
+readExpr input = case parse parseExpr "lisp" input of 
         Left err -> "Error: " ++ show err
         Right output -> "Found"
 
